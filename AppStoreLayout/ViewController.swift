2aa2d25f39462e4150b97c217b86c424781957cd
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2023-11-10 12:56:25 -0800

    Added .standard case to switch

--- AppStoreLayout/ViewController.swift
+++ AppStoreLayout/ViewController.swift
@@ -21,16 +21,80 @@ class ViewController: UIViewController {
         
         // MARK: Collection View Setup
         collectionView.collectionViewLayout = createLayout()
-                
+        collectionView.register(PromotedAppCollectionViewCell.self, forCellWithReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier)
+
+        collectionView.register(StandardAppCollectionViewCell.self, forCellWithReuseIdentifier: StandardAppCollectionViewCell.reuseIdentifier)
         configureDataSource()
     }
     
     func createLayout() -> UICollectionViewLayout {
-        fatalError("TODO: Create Layout")
+        let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
+            let section = self.sections[sectionIndex]
+            switch section {
+            case .promoted:
+                //MARK: Promoted Section Layout
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(300))
+                let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                
+                return section
+            case .standard:
+                // Mark: Standard Section Layout
+                let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalHeight(1/3))
+                let item = NSCollectionLayoutItem(layoutSize: itemSize)
+                //item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 4, bottom: 0, trailing: 4)
+                let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.92), heightDimension: .estimated(250))
+                let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 3)
+                let section = NSCollectionLayoutSection(group: group)
+                section.orthogonalScrollingBehavior = .groupPagingCentered
+                
+                return section
+            default:
+                return nil
+            }
+        }
+        return layout
     }
     
     func configureDataSource() {
-        fatalError("TODO: Create Data Source")
+        // MARK: Initialize Data Source
+        dataSource = .init(collectionView: collectionView, cellProvider: { (collectionView, indexPath, item) -> UICollectionViewCell? in
+            let section = self.sections[indexPath.section]
+            switch section {
+            case .promoted:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PromotedAppCollectionViewCell.reuseIdentifier, for: indexPath) as! PromotedAppCollectionViewCell
+                cell.configureCell(item.app!)
+                
+                return cell
+            case .standard:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: StandardAppCollectionViewCell.reuseIdentifier, for: indexPath) as! StandardAppCollectionViewCell
+                let isThirdItem = (indexPath.row + 1).isMultiple(of: 3)
+                cell.configureCell(item.app!, hideBottomLine: isThirdItem)
+
+                return cell
+            default:
+                fatalError("Not yet implemented")
+            }
+        })
+        
+        // MARK: Snapshot Definition
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.promoted])
+        snapshot.appendItems(Item.promotedApps, toSection: .promoted)
+        
+        let popularSection = Section.standard("Popular this week")
+        let essentialSection = Section.standard("Essential picks")
+        
+        snapshot.appendSections([popularSection, essentialSection])
+        snapshot.appendItems(Item.popularApps, toSection: popularSection)
+        snapshot.appendItems(Item.essentialApps, toSection: essentialSection)
+        
+        sections = snapshot.sectionIdentifiers
+        dataSource.apply(snapshot)
     }
 }
 
